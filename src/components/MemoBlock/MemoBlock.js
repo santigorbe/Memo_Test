7b19ea1e9cf46ffc7e31853c9ff4aed3419525da
santiago <santigorbe@gmail.com>
2021-08-19 13:25:18 -0300

    Version 1.0

---
+++ src/components/MemoBlock/MemoBlock.js
@@ -0,0 +1,19 @@
+import './MemoBlock.css';
+
+
+const MemoBlock = ({animating, handleMemoClick, memoBlock}) =>{
+    return(
+    <div className='memo-block' onClick={() => (!memoBlock.flipped && !animating) && handleMemoClick(memoBlock)} >
+            <div className={`memo-block-inner ${memoBlock.flipped && 'memo-block-flipped'}`}>
+                    <div className='memo-block-front'>
+                    </div>
+                    <div className='memo-block-back'>
+                        {memoBlock.emoji}
+                    </div>
+            </div>
+    </div>
+
+    )
+}
+
+export default MemoBlock;
